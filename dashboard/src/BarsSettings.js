f14a3270368ec083f79315db6c6b4c8a2016cfeb
seemywings <kevin.jayne@icloud.com>
2024-05-05 17:45:32 -0400

    comments

--- dashboard/src/BarsSettings.js
+++ dashboard/src/BarsSettings.js
@@ -5,6 +5,8 @@ function BarsSettings({ config, updateConfig }) {
     return (
         <div className="setting">
             <BrightnessSlider config={config} updateConfig={updateConfig} />
+            // add slider for sensitivity
+        // add button to save the configuration
         </div>
     );
 }
