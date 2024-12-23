afaf8be32b4c2d1a93e31ef426d344a1f2b205ed
seemywings <kevin.jayne@icloud.com>
2024-12-23 04:20:45 -0500

    closer to parity

--- LitBox.ino
+++ LitBox.ino
@@ -9,6 +9,11 @@
 // Web Server Config
 ESPWiFi wifi;
 
+// Visualization Config
+const int maxFrameRate = 120;
+unsigned int frameRate = 60;
+String visualization = "bars";
+
 void setup() {
   initializeMatrix();
   initializeWebServer();
@@ -17,22 +22,24 @@ void setup() {
   Serial.println("Lit Box Initialized");
 }
 
-void loop() { runAtFrameRate(drawBars, 60); }
+void loop() {
+  if (visualization == "gameOfLife") {
+    runAtFrameRate(drawGameOfLife, frameRate);
+  } else {
+    runAtFrameRate(drawBars, frameRate);
+  }
+}
 
 void drawBars() {
   spectralAnalyzer(LED_WIDTH, LED_HEIGHT);
   FastLED.clear();
-  // Check for new spectral data
-  // for (int x = 0; x < LED_WIDTH; x++) {
-  //   for (int y = 0; y < spectralData[x]; y++) {
-  //     RgbColor pixelColor = colorPallet[0];
-  //     pixelColor = (y > 1) ? colorPallet[1] : pixelColor;
-  //     pixelColor = (y > 3) ? colorPallet[2] : pixelColor;
-  //     pixelColor = (y > 6) ? colorPallet[3] : pixelColor;
-  //     drawPixel(x, y, pixelColor);
-  //   }
-  // }
-  drawPixel(16, 4, RED);
+  for (int x = 0; x < LED_WIDTH; x++) {
+    for (int y = 0; y < spectralData[x]; y++) {
+      // Map y to colorPallet index
+      CRGB pixelColor = colorPallet[min(y / 2, 3)];
+      drawPixel(x, y, pixelColor);
+    }
+  }
   FastLED.show();
 }
 
